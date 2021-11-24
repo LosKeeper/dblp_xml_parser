168e60dddf65708d1aae1ecf0c247932b69b3ebb
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2021-11-24 12:22:48 +0100

    affichage balise a la ligne auteurs et titres

--- parsage.c
+++ parsage.c
@@ -8,7 +8,7 @@ typedef struct parse {
 
 int main(int argc, char ** argv){
     FILE * entree=fopen(argv[1],"r");
-    if(!argc){
+    if(argc < 1 || argc > 2){
         return 0;
     }
     do{
@@ -19,8 +19,14 @@ int main(int argc, char ** argv){
             while(!strstr(buffer,"</article>")){
                 fgets(buffer,1000,entree);
                 char * author = strstr(buffer,"<author>");
+                char * title = strstr(buffer,"<title>");
                 if(author){
-                    printf("%s",author);
+                    strstr(buffer,"</author>")[0]='\0';
+                    printf("Auteur : %s\n",author+8);
+                }
+                if(title){
+                    strstr(buffer,"</title>")[0]='\0';
+                    printf("Titre : %s\n",title+7);
                 }
             }
             printf("------------------------------------------------\n\n");
