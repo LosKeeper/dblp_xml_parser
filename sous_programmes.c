29eddadb249885c53164bb4044350ef1a81a3a3a
Metzger Benjamin <benjamin.metzger@etu.unistra.fr>
2021-11-24 14:48:18 +0100

    co-auteurs

--- sous_programmes.c
+++ sous_programmes.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 int co_auteurs(char * auteur1, char *auteur2){
+    int k=0;
     FILE* fichier=NULL; 
     fichier=fopen(fichierthomas, "r+");
     if(!strcmp(auteur1,auteur2)){
@@ -12,10 +13,20 @@ int co_auteurs(char * auteur1, char *auteur2){
         return 0;
     }
     else{
-        char* marqueur;
-        while(fgets(marqueur,1000, fichier)!=EOF){
-            char *chaine=strstr(marqueur, ";")+1;
-        }
+        char buffer[1000];
+        fgets(buffer,1000, fichier)
+        char chaine1[1000];
+        char chaine2[1000];
+        strstr(buffer,";")+1;
+        strcpy(chaine1,buffer);
+        strcpy(chaine2,buffer);
+        do{
+            strstr(chaine1,";")[k]='\0';
+            strstr(chaine2,";")[k+1]='\0';
+            k+=1;
+        }while(buffer!=\0);
+       // char *chaine1=strstr(buffer, ";")+1;
+        //char *chaine2=strstr(buffer, ";")+1;
     }
     fclose(fichier);
 }
