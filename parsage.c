c5f8f6d5b5d4f449c1ddff8b83030e666f72a0be
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2021-11-24 11:55:07 +0100

    fonction detection auteurs avec balises a la ligne

--- parsage.c
+++ parsage.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 typedef struct parse {
     char * title;
@@ -8,11 +8,22 @@ typedef struct parse {
 
 int main(int argc, char ** argv){
     FILE * entree=fopen(argv[1],"r");
+    if(!argc){
+        return 0;
+    }
     do{
-        char * buffer[1000];
+        char buffer[1000];
         fgets(buffer,1000,entree);
-        if(strstr(buffer, "</article>")){
-
+        if(strstr(buffer, "<article")){
+            printf("ARTICLE\n------------------------------------------------\n");
+            while(!strstr(buffer,"</article>")){
+                fgets(buffer,1000,entree);
+                char * author = strstr(buffer,"<author>");
+                if(author){
+                    printf("%s",author);
+                }
+            }
+            printf("------------------------------------------------\n\n");
         }
     }while(!feof(entree));
 }
