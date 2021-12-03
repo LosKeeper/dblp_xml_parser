d5c52504b1fd4a7b4e3e48de956a9d022058e012
Metzger Benjamin <benjamin.metzger@etu.unistra.fr>
2021-12-03 18:41:41 +0100

    amélioration de ex_parse.c

--- ex_parse.c
+++ ex_parse.c
@@ -3,6 +3,7 @@
 
 
 int main(void){
+    int k;
     FILE * entree=fopen("ex_parse.txt","r");
     char buffer[1000];
     fgets(buffer,1000,entree);
@@ -11,6 +12,22 @@ int main(void){
     strcpy(titre,buffer);
     strstr(titre,";")[0]='\0';
     strstr(buffer,";")[8]='\0';
+    for(k=0; k<sizeof(buffer); k++)
+    {
+        if(buffer[k]!=';')
+        {
+            /**
+             * faire une fonction annexe qui retire le caractère lu a l'iteration par le k.
+             */
+        }
+        else
+        {
+            /**
+             * si on lit le caractère ';' alors on retire le k-ième caractère (le ;) puis renvoie le buffer don le deuxième
+             */
+        }
+        
+    }
     printf("%s\n",buffer);
     printf("%s\n",titre);  
 }
