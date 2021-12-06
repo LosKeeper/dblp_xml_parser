481aae5cd959a1b65d75034c97aeff26f2ba131d
Metzger Benjamin <benjamin.metzger@etu.unistra.fr>
2021-12-06 10:19:30 +0100

    projet avancement

--- ex_parse.c
+++ ex_parse.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 int main(void){
-    int k;
+    int k=0;
     FILE * entree=fopen("ex_parse.txt","r");
     char buffer[1000];
+    char buffercpy[1000];
+    strcpy(buffercpy,buffer);
     fgets(buffer,1000,entree);
     char titre[100];
     strcpy(buffer-1,strstr(buffer, ";"));
     strcpy(titre,buffer);
     strstr(titre,";")[0]='\0';
     strstr(buffer,";")[8]='\0';
-    for(k=0; k<sizeof(buffer); k++)
-    {
-        if(buffer[k]!=';')
-        {
-            /**
-             * faire une fonction annexe qui retire le caractère lu a l'iteration par le k.
-             */
-        }
-        else
-        {
-            /**
-             * si on lit le caractère ';' alors on retire le k-ième caractère (le ;) puis renvoie le buffer don le deuxième
-             */
-        }
-        
-    }
+    while(k<strlen(titre)){
+        buffercpy[k]= buffercpy[k+1];
+        buffercpy[strlen(buffercpy)-1]='\0';
+        k+=1;   
+}
     printf("%s\n",buffer);
-    printf("%s\n",titre);  
+    printf("%s\n",titre);
+    printf("%ld\n",strlen(titre));
+    printf("%s\n",buffercpy);
 }
