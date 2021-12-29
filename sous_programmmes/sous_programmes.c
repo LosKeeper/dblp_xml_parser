8920b6fae9c924355b50ea4c26d86f51bfcaa8c8
Metzger Benjamin <benjamin.metzger@etu.unistra.fr>
2021-12-29 14:46:10 +0100

    projet programm

--- sous_programmmes/sous_programmes.c
+++ sous_programmmes/sous_programmes.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+
+int nmbre_separateur(char* string){
+    int k=0;
+    int somme=0;
+    FILE* fichier=NULL;
+    fichier=fopen(fichierthomas, "r+");
+    char buffer[1000];
+    fgets(buffer,1000,fichier);
+    while(string[k]!='\0'){
+        if(string[k]==';'){
+            somme++;
+            k++;
+        }
+        else{
+            k++;
+        }
+    }
+
+}
+
+int nombres_comparaisons_a_realiser(){
+    int k;
+    int somme=nmbre_separateur(fichierthomas);
+    int nombre_comparaisons=0;
+    for(k=1; k<somme-1;k++){
+        nombre_comparaisons+=k;
+    }
+    return nombre_comparaisons;
+}
+
 /**
 int inverser_chaine(char* auteurs){
         char str[100], rev[100];
@@ -22,21 +52,19 @@ int inverser_chaine(char* auteurs){
 }
 */
 
-void inverser_ordre_auteurs(char* liste_auteurs){
-    
-}
+
 int co_auteurs(char * auteur1, char *auteur2){
     int k=0;
     FILE* fichier=NULL; 
     fichier=fopen(fichierthomas, "r+");
     if(!strcmp(auteur1,auteur2)){
         printf("C'est le même auteur\n");
-        printf("Le chemin le plus court est de 0")
+        printf("Le chemin le plus court est de 0");
         return 0;
     }
     else{
         char buffer[1000];
-        fgets(buffer,1000, fichier)
+        fgets(buffer,1000, fichier);
         char chaine1[100];
         char chaine2[100];
         strcpy(buffer-1,strstr(buffer, ";"));
