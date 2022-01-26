4885375f2cef77e3282f4aeab9c709bb077365b8
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2022-01-26 14:45:26 +0100

    tests

--- tests/tests.c
+++ tests/tests.c
@@ -29,10 +29,21 @@ int main(void) {
     compareFile("test_export_binary/my_export.txt",
                 "test_export_binary/file_exported.txt");
 
+    // test decode_html
+    char encoded[] = "M. Tamer &Ouml;zsu";
+    char decoded[] = "M. Tamer Ozsu";
+    decode_html(encoded);
+    fprintf(stderr, "Test decode html : ");
+    if (!strcmp(encoded, decoded)) {
+        fprintf(stderr, "\x1B[32mOK !\x1B[0m\n");
+    } else {
+        fprintf(stderr, "\x1B[31mNot OK !\x1B[0m\n");
+    }
+
     // test import binary file
     graphe_t graphe2;
     initGraphe(&graphe2);
-    FILE *entree2 = fopen("test_import_binary/file_to_import.txt", "qr");
+    FILE *entree2 = fopen("test_import_binary/file_to_import.txt", "r");
     FILE *sortie2 = fopen("test_import_binary/my_export.txt", "w");
     importGraphe(&graphe2, entree2);
     printGraphe(&graphe2, sortie2);
