4dec529f017be4c98213c3b531040070e2d0cba2
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2022-01-26 14:33:04 +0100

    rangement + couleurs tests

--- tests/tests.c
+++ tests/tests.c
@@ -13,7 +13,6 @@
 
 int main(void) {
     // test write binary file from an another file
-    fprintf(stderr, "Test write binary file : \n");
     graphe_t graphe;
     initGraphe(&graphe);
     data_t data;
@@ -26,18 +25,18 @@ int main(void) {
     FILE *sortie = fopen("test_export_binary/my_export.txt", "w");
     printGraphe(&graphe, sortie);
     fclose(sortie);
+    fprintf(stderr, "Test write binary file : ");
     compareFile("test_export_binary/my_export.txt",
                 "test_export_binary/file_exported.txt");
-    // free(&graphe);
 
     // test import binary file
     graphe_t graphe2;
     initGraphe(&graphe2);
-    fprintf(stderr, "Test import binary file : \n");
     FILE *entree2 = fopen("test_import_binary/file_to_import.txt", "qr");
     FILE *sortie2 = fopen("test_import_binary/my_export.txt", "w");
     importGraphe(&graphe2, entree2);
     printGraphe(&graphe2, sortie2);
+    fprintf(stderr, "Test import binary file : ");
     compareFile("test_import_binary/file_to_import.txt",
                 "test_import_binary/my_export.txt");
     fclose(entree2);
